1f4f3b0e593c4baf7878c2e68fb29176e410e83e
singhakshay29 <akshay2898.as@gmail.com>
2023-11-04 13:55:06 +0530

    Changes

--- src/app.js
+++ src/app.js
@@ -1,22 +1,31 @@
-const http = require('http');
+const http = require("http");
 
 const server = http.createServer((req, res) => {
-  if (req.method === 'POST') {
+  if (req.method === "POST") {
     const chunks = [];
 
-    req.on('data', chunk => {
+    req.on("data", (chunk) => {
       const buf = Buffer.from(chunk);
       const str = buf.toString();
       chunks.push(str);
-      const obj = JSON.parse(chunks)
+      const obj = JSON.parse(chunks);
       const value1 = obj.num1;
       const value2 = obj.num2;
 
       // Write code here to calculate power of a number
-      
+      if (!value1 || !value2) {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        res.end("The operation cannot be performed");
+      } else if (value1 <= 0) {
+        res.writeHead(400, { "Content-Type": "text/html" });
+        res.end("The operation cannot be performed");
+      } else {
+        const ans = Math.pow(value1, value2);
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end("Result" + ans);
+      }
     });
-    }
+  }
 });
 
 module.exports = server;
-      
